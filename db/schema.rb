421a77bf74e5b36cde44ec1930b4c6d9edcd6588
Alyssa Redman <alyred3@gmail.com>
2019-08-01 03:06:45 +0000

    created place form

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_07_31_014818) 
+ActiveRecord::Schema.define(version: 2019_07_31_014818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "places", id: :serial, force: :cascade do |t|
-  create_table "places", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "address"
