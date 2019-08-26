cd7e1667956eb2e2cab35265a2ede65ce93d68e0
Alyssa Redman <alyred3@gmail.com>
2019-08-26 04:58:16 +0000

    made adjustments to photos and captions

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2019_08_16_031244) do
     t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
-
   create_table "places", id: :serial, force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -55,7 +54,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_031244) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remembser_created_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sign_in_count", default: 0, null: false
