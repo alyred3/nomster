026bb68675b7f166388d58c3af042ceb920ec19e
Alyssa Redman <alyred3@gmail.com>
2019-08-27 23:39:37 +0000

    readjusted production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'nomster-alyssa-redman.herokuapp.com',
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
   
