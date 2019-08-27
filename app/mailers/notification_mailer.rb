80521290b3b7a998e740675ec44b3613409b75f9
Alyssa Redman <alyred3@gmail.com>
2019-08-27 23:26:35 +0000

    changed homepage slider

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,10 +2,9 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added(comment)
-    @place = comment.place
+    @place = @comment.place
     @place_owner = @place.user
-    mail(to: "@place_owner.email",
-        subject: "A comment has been added to #{@place.name}")
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
   end
-
 end
